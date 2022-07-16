c7f3662f1c8a94dc710861948a3e32fbcd820551
Danillo Souza <danillosouza1704@gmail.com>
2022-07-16 18:22:35 -0300

    Corrigido regex de teste

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
-  ],
-  testRegex: "((\\.|/*.)(spec))\\.ts?$",
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  testRegex: '((\\.|/*.)(spec))\\.tsx?$',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-}
+};
