9077f8a4e9de8627164d58259df98fc5e9dc1ebd
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-07-16 13:08:24 -0300

    #105 - Ajusta env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    SERVICE_URL: process.env.SERVICE_URL
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
