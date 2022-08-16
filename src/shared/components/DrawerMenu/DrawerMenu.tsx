3f1c64eca23db42918f44e376d7eeaf9d0190d3a
Danillo Souza <danillosouza1704@gmail.com>
2022-08-15 22:45:39 -0300

    Criado comoponente de Drawer

---
+++ src/shared/components/DrawerMenu/DrawerMenu.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import { Button, ButtonProps } from '@mui/material';
+import * as Styles from './styles';
+
+const { ScrollDiv } = Styles;
+
+interface Buttons {
+  label: string;
+  onClick: () => void;
+  backgroundColor: string;
+  color: string;
+  variant?: ButtonProps['variant'];
+}
+
+interface DrawerMenuProps {
+  children: JSX.Element;
+  open: boolean;
+  buttons?: Array<Buttons>;
+}
+
+const DrawerMenu = ({ children, open, buttons }: DrawerMenuProps) => {
+  const renderButtons = () => {
+    if (buttons)
+      return (
+        <Box marginTop="16px">
+          {buttons.map((button) => (
+            <Button
+              variant={button.variant}
+              sx={{
+                backgroundColor: button.backgroundColor,
+                color: button.color,
+                padding: '9px 20px',
+                marginRight: '16px'
+              }}
+              onClick={button.onClick}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </Box>
+      );
+  };
+
+  return (
+    <Drawer anchor="right" open={open}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        sx={{ padding: '46px 24px 34px', height: '100%' }}
+      >
+        <ScrollDiv>{children}</ScrollDiv>
+        {renderButtons()}
+      </Box>
+    </Drawer>
+  );
+};
+
+export default DrawerMenu;
---
+++ src/shared/components/DrawerMenu/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ScrollDiv = styled.div`
+  width: auto;
+  flexdirection: column;
+  overflow-y: auto;
+  margin-top: 50px;
+`;
