6a566eddd957b6f917b78177c87055f9475fe114
heronsousa <heron.sousa@sensedata.com.br>
2022-08-29 22:14:58 -0300

    fix Drawer styles

--- src/shared/components/DrawerMenu/styles.ts
+++ src/shared/components/DrawerMenu/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const ScrollDiv = styled.div`
   width: auto;
-  flexdirection: column;
+  flex-direction: column;
   overflow-y: auto;
-  margin-top: 50px;
 `;
