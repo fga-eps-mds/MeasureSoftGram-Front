a581d6d60d53f0bd45c08314e7eaaa5598bee1ca
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-09-01 17:47:08 -0300

    #224 - Adicionando integração com o componente de Filtros no ProjectContent

--- src/pages/projects/components/ProjectContent/ProjectContent.tsx
+++ src/pages/projects/components/ProjectContent/ProjectContent.tsx
@@ -7,11 +7,14 @@ import { Box, IconButton, Menu, MenuItem, Typography } from '@mui/material';
 
 import { Project } from '@customTypes/project';
 
+import Filters from '@components/Filters';
+
 import CreateRelease from '@pages/createRelease';
 import { MoreVert } from '@mui/icons-material';
 import Skeleton from '../Skeleton';
 
-import Circle from './styles';
+import { BodyContainer, Circle, FiltersContainer } from './styles';
+import { filterOptions } from './filterOptions';
 
 interface Props {
   project?: Project;
@@ -31,7 +34,7 @@ const ProjectContent: React.FC<Props> = ({ project }) => {
   };
 
   const handleOpenCreateRelease = () => {
-    setOpenCreateRelease(true)
+    setOpenCreateRelease(true);
     setAnchorEl(null);
   };
 
@@ -65,13 +68,21 @@ const ProjectContent: React.FC<Props> = ({ project }) => {
             open={openMenu}
             onClose={handleCloseMenu}
             MenuListProps={{
-              'aria-labelledby': 'basic-button',
+              'aria-labelledby': 'basic-button'
             }}
           >
             <MenuItem onClick={handleOpenCreateRelease}>Definir release</MenuItem>
           </Menu>
         </Box>
       </Box>
+
+      <BodyContainer display="flex" flexDirection="row">
+        <FiltersContainer display="flex" flexDirection="column">
+          {filterOptions.map((filter) => (
+            <Filters key={filter.filterTitle} filterTitle={filter.filterTitle} options={filter.options} />
+          ))}
+        </FiltersContainer>
+      </BodyContainer>
 
       <CreateRelease
         open={openCreateRelease}
