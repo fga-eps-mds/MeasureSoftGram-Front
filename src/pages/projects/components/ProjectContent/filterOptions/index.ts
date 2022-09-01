c4232b4a3f2fe964bd5ec8328576de1f6db6e871
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-09-01 17:46:45 -0300

    #224 - Adicionando opções dos filtros  estáticos

---
+++ src/pages/projects/components/ProjectContent/filterOptions/index.ts
@@ -0,0 +1,14 @@
+export const filterOptions = [
+  {
+    filterTitle: 'CARACTERÍSTICAS',
+    options: [{ optionTitle: 'Manutenabilidade' }, { optionTitle: 'Produtividade' }, { optionTitle: 'Confiabilidade' }]
+  },
+  {
+    filterTitle: 'SUB CARACTERÍSTICAS',
+    options: [
+      { optionTitle: 'Sub características1' },
+      { optionTitle: 'Sub características2' },
+      { optionTitle: 'Sub características3' }
+    ]
+  }
+];
