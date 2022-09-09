d7e0db6a0acdccee86190e1f3e40d740a1a46adc
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-09-09 11:59:04 -0300

    #224 - Add feature to filter charts accordly options selected

--- src/shared/components/Filters/Filters.tsx
+++ src/shared/components/Filters/Filters.tsx
@@ -4,18 +4,27 @@ import { ExpandCircleDown, ExpandCircleDownOutlined } from '@mui/icons-material'
 import { Checkbox } from '@mui/material';
 import { FiltersTitle, Option, OptionContainer } from './styles';
 
-interface OptionProps {
-  name: string;
-  key: string;
+interface OptionCheckedProps {
+  [key: string]: boolean;
 }
 
 interface FiltersProps {
   filterTitle: string;
-  options?: Array<OptionProps>;
+  options?: Array<string>;
+  updateOptions: Function;
+  checkedOptions: OptionCheckedProps;
 }
 
-const Filters: React.FC<FiltersProps> = ({ filterTitle, options = [] }) => {
+const Filters: React.FC<FiltersProps> = ({ filterTitle, options = [], updateOptions, checkedOptions }) => {
   const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const handleUpdate = (selectedOption: string) => {
+    updateOptions({
+      ...checkedOptions,
+      [selectedOption]: !checkedOptions[selectedOption]
+    });
+  };
+
   return (
     <div>
       <FiltersTitle onClick={() => setIsVisible(!isVisible)}>
@@ -26,9 +35,9 @@ const Filters: React.FC<FiltersProps> = ({ filterTitle, options = [] }) => {
       {isVisible ? (
         <OptionContainer display="flex" flexDirection="column">
           {options.map((option) => (
-            <Option key={option.key} display="flex" flexDirection="row" alignItems="center">
-              <Checkbox onChange={() => console.log(`oi ${option.name}`)} />
-              <span>{option.name}</span>
+            <Option key={option} display="flex" flexDirection="row" alignItems="center">
+              <Checkbox onChange={() => handleUpdate(option)} />
+              <span>{option[0].toUpperCase() + option.slice(1).replace('_', ' ')}</span>
             </Option>
           ))}
         </OptionContainer>
