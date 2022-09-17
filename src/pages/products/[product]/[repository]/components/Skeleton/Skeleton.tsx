ef3842db65a3ebd2f99611e65495725d7cedda92
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-09-17 04:41:46 -0300

    Add Repository Skeleton

--- src/pages/products/[product]/[repository]/components/Skeleton/Skeleton.tsx
+++ src/pages/products/[product]/[repository]/components/Skeleton/Skeleton.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { range } from 'lodash';
 
-import { Box, Skeleton as Shimmer } from '@mui/material';
+import { Box, Container, Skeleton as Shimmer } from '@mui/material';
+
+import * as Styles from '../../styles';
 
 function Skeleton() {
   return (
-    <Box display="flex" flexDirection="column">
-      <Box display="flex" flexDirection="row" alignItems="center" marginY="60px">
-        <Box marginRight="20px">
-          <Shimmer variant="circular" height={30} width={30} />
+    <Box display="flex" width="100%" flexDirection="row">
+      <Styles.FilterBackground>
+        <Box display="flex" paddingX="15px" flexDirection="column" marginTop="36px" position="fixed">
+          {range(2).map(() => (
+            <Box paddingBottom="10px">
+              <Shimmer variant="text" height={32} width={200} />
+              <Box display="flex" flexDirection="column" alignItems="end">
+                <Shimmer variant="text" height={25} width={180} />
+                <Shimmer variant="text" height={25} width={180} />
+              </Box>
+            </Box>
+          ))}
+        </Box>
+      </Styles.FilterBackground>
+
+      <Container>
+        <Box display="flex" flexDirection="row" alignItems="center" marginTop="40px" marginBottom="36px">
+          <Box>
+            <Shimmer variant="text" height={50} width={400} />
+            <Shimmer variant="text" height={16} width={200} />
+          </Box>
         </Box>
 
         <Box>
-          <Shimmer variant="text" height={32} width={300} />
-          <Shimmer variant="text" height={14} width={200} />
+          <Shimmer variant="rectangular" height={280} />
         </Box>
-      </Box>
+      </Container>
     </Box>
   );
 }
