477330db2c4fa8a8dbb78c71c4f35273d3286ad4
heronsousa <heron.sousa@sensedata.com.br>
2022-09-18 17:48:35 -0300

    fix sqc products history

--- src/shared/utils/formatRepositoriesSqcHistory.ts
+++ src/shared/utils/formatRepositoriesSqcHistory.ts
@@ -14,20 +14,60 @@ const formatTwoDecimalPlaces = (value: number) => Math.round(value * 100) / 100;
 const formatRepositoriesSqcHistory = (history: RepositoriesSqcHistory) => {
   const legendData: string[] = [];
 
-  const xAxisData = history.results[longestHistoryMetricsIndex(history.results)].history.map((metric) =>
-    format(new Date(metric.created_at.replace(/\s+/g, '')), 'dd/MM/yyyy HH:MM')
-  );
+  const longestHistory = history.results[longestHistoryMetricsIndex(history.results)]
 
-  const series = history.results.map((item) => {
-    legendData.push(item.name);
+  const dates = [...new Set(longestHistory.history.map(item => item.created_at))]
 
-    return {
-      name: item.name,
-      data: item.history.map((metric) => formatTwoDecimalPlaces(metric.value)),
+  history.results.forEach((item, index) => {
+    if (index === longestHistoryMetricsIndex(history.results)) return;
+
+    [...new Set(item.history.map(item => item.created_at))].forEach(i => {
+      if (!dates.includes(i)) dates.push(i)
+    })
+  })
+
+  dates.sort((a, b) => new Date(a) > new Date(b))
+
+
+  const highestOccrs = (date: string) => history.results.reduce((amount, repo) => {
+      const highest =  repo.history.reduce((acc, metric) => acc + Number(metric.created_at === date), 0)
+
+      return Math.max(amount, highest)
+    }, 0)
+
+  let xAxisData: string[] = new Array(200)
+  let initialFillIndex = 0
+
+  dates.forEach(date => {
+    xAxisData.fill(date, initialFillIndex, initialFillIndex + highestOccrs(date))
+
+    initialFillIndex += highestOccrs(date)
+  })
+
+  xAxisData = xAxisData.filter(item => item)
+
+  const seriesAux = []
+
+  history.results.forEach((repo, index) => {
+    seriesAux.push({
+      name: repo.name,
+      data: new Array(xAxisData.length).fill(null),
       type: 'line',
       animationDuration: 1200
-    };
-  });
+    })
+
+    legendData.push(repo.name);
+
+    // eslint-disable-next-line no-plusplus
+    for(let i=0, j=0; j<repo.history.length; j++, i++) {
+      if (xAxisData[i] === repo.history[j].created_at) {
+        seriesAux[index].data[i] = formatTwoDecimalPlaces(repo.history[j].value)
+      } else {
+        i = xAxisData.findIndex(item => item == repo.history[j].created_at)
+        seriesAux[index].data[i] = formatTwoDecimalPlaces(repo.history[j].value)
+      }
+    }
+  })
 
   return {
     title: {
@@ -59,25 +99,27 @@ const formatRepositoriesSqcHistory = (history: RepositoriesSqcHistory) => {
     dataZoom: [
       {
         type: 'inside',
-        start: xAxisData.length - 20,
+        start: 0,
         end: xAxisData.length - 1
       },
       {
-        start: xAxisData.length - 20,
+        start: 0,
         end: xAxisData.length - 1
       }
     ],
     xAxis: {
       type: 'category',
-      data: xAxisData,
+      data: xAxisData, // .map(metric => format(new Date(metric.replace(/\s+/g, '')), 'dd/MM/yyyy HH:MM')),
       axisLabel: {
         rotate: 45
       }
     },
-    yAxis: {
-      type: 'value'
-    },
-    series
+    yAxis: [{
+      ticks: {
+        beginAtZero: false
+      }
+    }],
+    series: seriesAux
   };
 };
 
