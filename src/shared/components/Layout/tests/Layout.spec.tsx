fe68dc9a36388fa14a237f199225b71882b9d6fe
Danillo Souza <danillosouza1704@gmail.com>
2022-09-19 10:22:23 -0300

    Adicionado provider ao teste

--- src/shared/components/Layout/tests/Layout.spec.tsx
+++ src/shared/components/Layout/tests/Layout.spec.tsx
@@ -3,12 +3,17 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { OrganizationProvider } from '@contexts/OrganizationProvider';
 import Layout from '../Layout';
 
 describe('<Layout />', () => {
   describe('Snapshot', () => {
     it('Deve corresponder ao Snapshot', () => {
-      const tree = render(<Layout />);
+      const tree = render(
+        <OrganizationProvider>
+          <Layout />
+        </OrganizationProvider>
+      );
       expect(tree).toMatchSnapshot();
     });
   });
