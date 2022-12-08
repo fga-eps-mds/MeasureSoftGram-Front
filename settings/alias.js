de28f3e753b6494e7d0746489f46d562a0e989ce
NilvanPeres <nilvanperes@gmail.com>
2022-12-08 17:48:32 -0300

    #27 Add new route

--- settings/alias.js
+++ settings/alias.js
@@ -16,6 +16,10 @@ module.exports = [
     path: './src/pages'
   },
   {
+    name: '@modules',
+    path: './src/modules'
+  },
+  {
     name: '@services',
     path: './src/shared/services'
   },
