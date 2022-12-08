9a680e14ef96f4b31224f55f1fdc979a6fcad627
luucas-melo <lucasmelodos322@gmail.com>
2022-12-08 19:56:16 -0300

    Handle login submit
    https://github.com/fga-eps-mds/2022-2-MeasureSoftGram-Doc/issues/46
    
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

--- src/pages/auth/components/SignInForm.tsx
+++ src/pages/auth/components/SignInForm.tsx
@@ -15,7 +15,7 @@ import { useForm } from 'react-hook-form';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const SignInForm = () => {
-  const { register } = useForm<LoginFormData>();
+  const { register, handleSubmit } = useForm<LoginFormData>();
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => {
@@ -24,33 +24,39 @@ export const SignInForm = () => {
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
+
+  const onSubmit = async (data: LoginFormData) => {
+    console.log('SIGNIN DATA', data);
+  };
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', gap: '2rem' }}>
-      <TextField label="Email" id="outlined-start-adornment" {...register('email')} />
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-          {...register}
-        />
-      </FormControl>
-      <Button type="submit" variant="contained">
-        Login
-      </Button>
-    </Box>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', gap: '2rem' }}>
+        <TextField label="Email" id="outlined-start-adornment" {...register('email')} />
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            {...register('password')}
+          />
+        </FormControl>
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </Box>
+    </form>
   );
 };
