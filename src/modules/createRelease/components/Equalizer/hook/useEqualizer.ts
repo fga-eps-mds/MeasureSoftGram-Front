8da25b41d8bbc1a546d4613b97064bc6c986ce5a
luucas-melo <lucasmelodos322@gmail.com>
2023-01-02 22:26:17 -0300

    Add array deps in hooks and define initial characteristic value as 50
    Co-authored-by: NilvanPeres  <nilvanperes@gmail.com>

--- src/modules/createRelease/components/Equalizer/hook/useEqualizer.ts
+++ src/modules/createRelease/components/Equalizer/hook/useEqualizer.ts
@@ -16,52 +16,58 @@ export default function useEqualizer(selectedCharacteristics: string[]) {
   const [valuesCommitted, setValuesCommitted] = useState<ValuesCommitted>(INITIAL_VALUES_COMMITTED);
   const [changes, setChanges] = useState<Changes[]>([]);
 
-  const addDeltaToChanges = useCallback((characteristicName: string, newValue: number) => {
-    const characteristic = characteristics.find((c) => c.key === characteristicName);
-    const characteristicDragged = characteristic!.key as keyof typeof valuesCommitted;
+  const addDeltaToChanges = useCallback(
+    (characteristicName: string, newValue: number) => {
+      const characteristic = characteristics.find((c) => c.key === characteristicName);
+      const characteristicDragged = characteristic!.key as keyof typeof valuesCommitted;
 
-    const value = valuesCommitted[characteristicDragged];
+      const value = valuesCommitted[characteristicDragged];
 
-    const delta = newValue - value;
+      const delta = newValue - value;
 
-    setChanges((prevChanges) => [...prevChanges, { characteristic_key: characteristicDragged, delta }]);
+      setChanges((prevChanges) => [...prevChanges, { characteristic_key: characteristicDragged, delta }]);
 
-    setValuesCommitted({
-      ...characteristics.reduce((acc, item) => ({ ...acc, [item.key]: item.value }), {}),
-      [characteristicDragged]: newValue
-    });
-  }, []);
+      setValuesCommitted({
+        ...characteristics.reduce((acc, item) => ({ ...acc, [item.key]: item.value }), {}),
+        [characteristicDragged]: newValue
+      });
+    },
+    [characteristics, valuesCommitted]
+  );
 
-  const equalize = useCallback((characteristicName: string, val: number) => {
-    const updatedCharacteristics = characteristics.map((item) => item);
+  const equalize = useCallback(
+    (characteristicName: string, val: number) => {
+      const updatedCharacteristics = characteristics.map((item) => item);
 
-    const characteristic = updatedCharacteristics.find((c) => c.key === characteristicName);
-    const { correlations, value } = characteristic!;
+      const characteristic = updatedCharacteristics.find((c) => c.key === characteristicName);
+      const { correlations, value } = characteristic!;
 
-    const delta = val - value;
+      const delta = val - value;
 
-    characteristic!.value = val;
+      characteristic!.value = val;
 
-    correlations['+'].forEach((characteristicKey) => {
-      const correlatedCharacteristic = updatedCharacteristics.find((item) => item.key === characteristicKey);
+      correlations['+'].forEach((characteristicKey) => {
+        const correlatedCharacteristic = updatedCharacteristics.find((item) => item.key === characteristicKey);
 
-      if (correlatedCharacteristic) {
-        const newValue = correlatedCharacteristic!.value + delta;
-        correlatedCharacteristic!.value = Math.max(0, Math.min(newValue, 100));
-      }
-    });
+        if (correlatedCharacteristic) {
+          const newValue = correlatedCharacteristic!.value + delta;
+          correlatedCharacteristic!.value = Math.max(0, Math.min(newValue, 100));
+        }
+      });
 
-    correlations['-'].forEach((characteristicKey) => {
-      const correlatedCharacteristic = updatedCharacteristics.find((item) => item.key === characteristicKey);
+      correlations['-'].forEach((characteristicKey) => {
+        const correlatedCharacteristic = updatedCharacteristics.find((item) => item.key === characteristicKey);
 
-      if (correlatedCharacteristic) {
-        const newValue = correlatedCharacteristic!.value - delta;
-        correlatedCharacteristic!.value = Math.max(0, Math.min(newValue, 100));
-      }
-    });
+        if (correlatedCharacteristic) {
+          const newValue = correlatedCharacteristic!.value - delta;
+          correlatedCharacteristic!.value = Math.max(0, Math.min(newValue, 100));
+        }
+      });
 
-    setCharacteristics(updatedCharacteristics);
-  }, []);
+      setCharacteristics(updatedCharacteristics);
+    },
+    [characteristics]
+  );
 
   return {
     changes,
