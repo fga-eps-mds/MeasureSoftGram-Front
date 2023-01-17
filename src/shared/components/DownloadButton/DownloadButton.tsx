0fa9f703a74896baecb44b0a7934d8395b1b882c
italofernandes13 <italoserra99@gmail.com>
2023-01-17 20:14:02 -0300

    add download functionality in .json file
    Co-authored-by: JoaoVictorValadao <joaovaladao161274@gmail.com>
    Co-authored-by: Ítalo Alves <italooko@gmail.com>

--- src/shared/components/DownloadButton/DownloadButton.tsx
+++ src/shared/components/DownloadButton/DownloadButton.tsx
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 
-const file = "/src/test2.py";
+const Download = () => {
+  const [loading, setLoading] = useState(false);
 
-export default function Download() {
-  const [download, setDownload] = useState("");
-  const [count, setCount] = useState(0);
+  const handleDownload = async () => {
+    setLoading(true);
+    try {
+        const response = await fetch('http://localhost:8080/api/v1/organizations/1/products/1/repositories/1/historical-values/sqc/');
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'relatorio_de_qualidade.json';
+        document.body.appendChild(link);
+        link.click();
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setLoading(false);
+    }
+  };
   return (
-    <div className="Download">
-      <p>
-        <Button
-          onClick={() => {
-            setDownload(file);
-            setCount((old) => old + 1);
-          }}variant="outlined" startIcon={<DownloadIcon />}
-        >
-          Download
-        </Button>
-      </p>
-      <p>{download}</p>
-    </div>
-  );
-}
+          <div className="Download">
+            <p>
+              <Button variant="outlined" startIcon={<DownloadIcon />}
+                onClick={handleDownload} disabled={loading}>
+                {loading ? 'Downloading...' : 'Download'}
+              </Button>
+            </p>
+          </div>
+        );
+};
+
+export default Download;
