9bbf1e48200e832acdc3231f7554f0d0816bc89c
italofernandes13 <italoserra99@gmail.com>
2023-01-20 09:24:07 -0300

    changing to download the chosen history
    Co-authored-by: Ítalo Alves <italooko@gmail.com>
    Co-authored-by: italovinicius18 <italovinicius18@yahoo.com>

--- src/shared/components/DownloadButton/DownloadButton.tsx
+++ src/shared/components/DownloadButton/DownloadButton.tsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import { Button, Tooltip } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 
-const Download = () => {
+
+const Download = (props: any) => {
   const [loading, setLoading] = useState(false);
 
   const handleDownload = async () => {
     setLoading(true);
     try {
-        const response = await fetch('http://localhost:8080/api/v1/organizations/1/products/1/repositories/1/historical-values/sqc/');
+        console.log(props.product.historical_values[props.kind])
+        const response = await fetch(props.product.historical_values[props.kind]);
         const blob = await response.blob();
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'relatorio_de_qualidade.json';
+        link.download = props.kind + 'Historical.json';
         document.body.appendChild(link);
         link.click();
     } catch (error) {
