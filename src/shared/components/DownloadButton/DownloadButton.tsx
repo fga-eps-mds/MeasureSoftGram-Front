da964fb49dcd9e98d5a5d411c4c4a5fa434bfc59
italofernandes13 <italoserra99@gmail.com>
2023-01-24 21:16:25 -0300

    fixing .json download functionality according to filter

--- src/shared/components/DownloadButton/DownloadButton.tsx
+++ src/shared/components/DownloadButton/DownloadButton.tsx
@@ -10,14 +10,41 @@ const Download = (props: any) => {
     setLoading(true);
     try {
         console.log(props.product.historical_values[props.kind])
-        const response = await fetch(props.product.historical_values[props.kind]);
-        const blob = await response.blob();
+        // const response = await fetch(props.product.historical_values[props.kind]);
+
+        const response = await fetch(props.product.historical_values[props.kind])
+        .then(response => response.json())
+        console.log('response', response);
+
+        const newProps = [];
+        for (var key in response.results) {
+          var item = response.results[key];
+          newProps.push({
+            id: item.id,
+            key: item.key,
+            name: item.name,
+            history: item.history
+          });
+        }
+        console.log('newProps', newProps);
+
+        const startDate = new Date(props.startDate);
+        const endDate = new Date(props.endDate);
+        const filteredResults = response.results.map(item => {
+            item.history = item.history.filter(historyItem => {
+                const historyDate = new Date(historyItem.created_at);
+                return historyDate >= startDate && historyDate <= endDate;
+            });
+            return item;
+        });
+        console.log('filtro', filteredResults);
+        const json = JSON.stringify(filteredResults);
+        const blob = new Blob([json], { type: "application/json" });
         const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = props.kind + 'Historical.json';
-        document.body.appendChild(link);
-        link.click();
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = props.kind + 'Historical.json';
+        a.click();
     } catch (error) {
         console.log(error);
     } finally {
