cfd8c2e4d50968a3bbc8480c8f6c47d3447f198b
NilvanPeres <nilvanperes@gmail.com>
2023-01-30 15:54:07 -0300

    tests: create tests to HistoricalLatestInfos component

---
+++ src/pages/products/[product]/[repository]/components/HistoricalInfosList/HistoricalLatestInfos/tests/HistoricalLatestInfos.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ProductProvider } from '@contexts/ProductProvider';
+import HistoricalLatestInfos from '../HistoricalLatestInfos';
+
+jest.mock('@contexts/ProductProvider', () => ({
+  useProductContext: () => ({
+    currentProduct: {
+      name: `aoba`
+    }
+  })
+}));
+
+describe('HistoricalLatestInfos component', () => {
+  it('should render without errors', () => {
+    const { container } = render(<HistoricalLatestInfos checkedOptions={{}} />);
+
+    expect(container).toBeInTheDocument();
+  });
+});
