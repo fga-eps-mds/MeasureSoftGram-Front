ccedb64b0248ee032e3875473b763c9f92e7dc96
Ítalo Alves <italooko@gmail.com>
2023-02-01 23:59:15 -0300

    Fixing lint and removing unused code in DownloadButton
    Co-authored-by: italofernandes13 <italoserra99@gmail.com>

--- src/shared/components/DownloadButton/DownloadButton.tsx
+++ src/shared/components/DownloadButton/DownloadButton.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import { Button, Tooltip } from "@mui/material";
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
-import { parse } from 'date-fns'
-
+import { parse } from 'date-fns';
 
 interface DownloadProps {
   product: any;
@@ -17,40 +16,30 @@ const Download = ({ product, kind, startDate, endDate }: DownloadProps) => {
   const handleDownload = async () => {
     setLoading(true);
     try {
-        const response = await fetch(product.historical_values[kind])
-          .then(response => response.json())
+      const res = await fetch(product.historical_values[kind]).then((response) => response.json());
+      const { results } = res;
 
-        const newProps = [];
-        for (var key in response.results) {
-          var item = response.results[key];
-          newProps.push({
-            id: item.id,
-            key: item.key,
-            name: item.name,
-            history: item.history
-          });
-        }
+      const parsedStartDate = parse(startDate, 'dd/MM/yyyy', new Date());
+      const parsedEndDate = parse(endDate, 'dd/MM/yyyy', new Date());
+      parsedStartDate.setHours(0, 0, 0, 0);
+      parsedEndDate.setHours(0, 0, 0, 0);
 
-        const parsedStartDate = parse(startDate, 'dd/MM/yyyy', new Date());
-        const parsedEndDate = parse(endDate, 'dd/MM/yyyy', new Date());
-        parsedStartDate.setHours(0, 0, 0, 0);
-        parsedEndDate.setHours(0, 0, 0, 0);
-
-        const filteredResults = response.results.map((item: any) => {
-          item.history = item.history.filter((historyItem: any) => {
-            const historyDate = new Date(historyItem.created_at);
-            historyDate.setHours(0, 0, 0, 0);
-            return (historyDate >= parsedStartDate) && (historyDate <= parsedEndDate);
-          });
-          return item;
+      const filteredResults = results.map((item: any) => {
+        const history = item.history.filter((historyItem: any) => {
+          const historyDate = new Date(historyItem.created_at);
+          historyDate.setHours(0, 0, 0, 0);
+          return historyDate >= parsedStartDate && historyDate <= parsedEndDate;
         });
-        const json = JSON.stringify(filteredResults);
-        const blob = new Blob([json], { type: "application/json" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = kind + 'Historical.json';
-        a.click();
+        return { ...item, history };
+      });
+
+      const json = JSON.stringify(filteredResults);
+      const blob = new Blob([json], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `${kind}Historical.json`;
+      a.click();
     } catch (error) {
       console.log(error);
     } finally {
@@ -60,8 +49,7 @@ const Download = ({ product, kind, startDate, endDate }: DownloadProps) => {
   return (
     <div className="Download">
       <p>
-        <Button variant="outlined" startIcon={<DownloadIcon />}
-          onClick={handleDownload} disabled={loading}>
+        <Button variant="outlined" startIcon={<DownloadIcon />} onClick={handleDownload} disabled={loading}>
           {loading ? 'Downloading...' : 'Download'}
         </Button>
       </p>
