7e705dd1d432140c11c3dae336d85a68cb50f743
higton <matpaulo@hotmail.com.br>
2023-05-20 10:03:56 -0300

    fix: add await to router push
    Co-authored-by: adrian <adriansoareslps@gmail.com>

--- src/shared/hooks/useRequireAuth.tsx
+++ src/shared/hooks/useRequireAuth.tsx
@@ -1,15 +1,24 @@
 import { useRouter } from "next/router";
 import { useAuth } from "@contexts/Auth";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const useRequireAuth = () => {
     const { session, loading } = useAuth();
     const router = useRouter();
-
-    if (loading === 'loaded' && !session && router.pathname !== '/') {
-        toast.error('Você não está autorizado a acessar esta página. Por favor, faça login.');
-        router.push('/');
-    }
-};
+  
+    useEffect(() => {
+      if (loading === "loaded" && !session && router.pathname !== "/") {
+        const redirect = async () => {
+          toast.error(
+            "Você não está autorizado a acessar esta página. Por favor, faça login."
+          );
+          await router.push("/");
+        };
+  
+        redirect();
+      }
+    }, [loading, session, router]);
+  };
 
 export default useRequireAuth;
