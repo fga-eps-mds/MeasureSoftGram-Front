8c0da996d7100c99491e62e75cee5f9570b55d6a
igorq937 <igorq937@gmail.com>
2023-05-26 17:46:51 -0300

    feat: add component SideMenuItem

---
+++ src/shared/components/Layout/SideMenu/SideMenuItem/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div<{ $collapsed?: boolean | null }>`
+  width: ${(props) => (props.$collapsed ? 'auto' : '290px')};
+  height: 45px;
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 4px;
+
+  color: #474747;
+  font-weight: 400;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 40px;
+`;
