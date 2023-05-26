0f30ebdabf3ee6a007f9c4a524ca622beb52c4d2
carlosFiuza <carlosfiuza1234@gmail.com>
2023-05-26 00:52:35 -0300

    refactor: change component home/CardInfo
    Co-authored-by:  Eddie Gomes <edvan.bgjunior@gmail.com>

--- src/pages/home/components/CardInfo/CardInfo.tsx
+++ src/pages/home/components/CardInfo/CardInfo.tsx
@@ -4,24 +4,24 @@ import Link from "next/link";
 
 import { Box, Typography, Card, Button, CardMedia, CardContent } from "@mui/material";
 
-import { InfoDataType } from "@customTypes/home";
+import { InfoData } from "@customTypes/home";
 
 interface Props {
-  cardData: Array<InfoDataType>,
+  cardData: InfoData,
 }
 
 const CardInfo: React.FC<Props> = ({ cardData }) => (
-  <Card id={cardData.at(0)?.title} sx={{ overflow: "visible", borderShadow: "none", borderRadius: "30px", display: "flex", flexDirection: "column", height: "100%", flex: "1" }}>
-    {cardData.map(data => (
-      <CardContent sx={{ display: "flex", height: "100%", flexDirection: "column", }}>
-        <Box display="flex" justifyContent="space-between" gap="2rem">
+  <Card id={cardData?.id} sx={{ overflow: "visible", borderShadow: "none", borderRadius: "30px", display: "flex", flexDirection: "column", height: "100%", flex: "1" }}>
+    {cardData.elements.map(data => (
+      <CardContent key={data.title} sx={{ display: "flex", height: "100%", flexDirection: "column", }}>
+        <Box display="flex" justifyContent="space-between" gap="1rem">
           <CardMedia
             component="img"
             image={data.imageSrc}
             alt="green iguana"
             sx={{ width: "50%", padding: "0 1rem 1rem 1rem", maxWidth: "400px", height: "auto", maxHeight: "200px", borderRadius: "30px" }}
           />
-          <Box display="flex" flexDirection="column" maxWidth="50%">
+          <Box display="flex" flexDirection="column" maxWidth="50%" alignItems="center" alignContent="center" width="100%">
             <Typography gutterBottom variant="h5" component="div" align="center">
               {data.title}
             </Typography>
--- src/shared/customTypes/home.ts
+++ src/shared/customTypes/home.ts
@@ -1,6 +1,11 @@
-export interface InfoDataType {
+export type infoDataType = {
   imageSrc: string,
   title: string,
   description: string,
   routeTo?: string,
+}
+
+export interface InfoData {
+  id: string,
+  elements: Array<infoDataType>,
 }
