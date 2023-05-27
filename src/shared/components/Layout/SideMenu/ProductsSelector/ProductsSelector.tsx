392f951723150f2e6bc49cd733141770f3359cb7
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-27 08:58:40 -0300

    feature: Open Products list and links

--- src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
+++ src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { SelectChangeEvent } from '@mui/material';
 import { FiBox, FiRepeat } from 'react-icons/fi';
@@ -7,12 +7,18 @@ import { useRouter } from 'next/router';
 import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import LetterAvatar from '@components/LetterAvatar';
 import SideMenuItem from '../SideMenuItem';
+import ListSide from '../ListSide/ListSide';
+import { Product } from '@customTypes/product';
 
 function ProductSelector() {
   const router = useRouter();
-  const { productsList, setCurrentProduct } = useProductContext();
+  const [openClose, setToggle] = useState(false)
+  const { productsList, setCurrentProduct, currentProduct } = useProductContext();
   const { currentOrganization } = useOrganizationContext();
 
+  const handleClose = () => {
+    setToggle(!openClose);
+  }
   const handleChange = useCallback(
     (event: SelectChangeEvent) => {
       const productId = Number(event.target.value);
@@ -20,20 +26,35 @@ function ProductSelector() {
       const product = productsList.find((productData) => Number(productData.id) === productId);
       if (product) {
         setCurrentProduct(product);
-        router.push(`/products/${currentOrganization?.id}-${product?.id}-${product?.name}`);
       }
     },
     [currentOrganization?.id, productsList, router, setCurrentProduct]
   );
 
+  const handleSetProduct = (item: Product) => {
+    let oldproduct = currentProduct;
+        setCurrentProduct(item);
+        if (oldproduct !== item) {
+          router.push(`/products/${currentOrganization?.id}-${item?.id}-${item?.name}`);
+        }
+  }
+
   const getProductId = useCallback((productName: string) => Number(productName.split('-')[1]) || '', []);
 
   return (
     <SideMenuItem
-      startIcon={<LetterAvatar name={currentOrganization?.name} icon={<FiBox />} />}
-      text={currentOrganization?.name}
+      optype='drawer'
+      context={'organization'}
+      openState={openClose}
+      handleClose={handleClose}
+      startIcon={<LetterAvatar name={currentProduct? currentProduct.name : 'S'} icon={<FiBox />} />}
+      text={currentProduct? currentProduct.name : 'Selecione Um Produto'}
       endIcon={<FiRepeat />}
       tooltip="Seleção de produto"
+      children={<ListSide orgs={productsList || []} 
+        title='Selecione o Produto' 
+        handleSelection={handleSetProduct}
+      />}
     />
   );
 }
