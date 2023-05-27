b88f6e5502ed808bac1207c59448f3ef5b864875
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-26 23:51:35 -0300

    List Drawer for organizations
    Co-authored-by: Igor <igorq937@users.noreply.github.com>

---
+++ src/shared/components/Layout/SideMenu/ListSide/ListSide.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from '@mui/material/'
+import LetterAvatar from '@components/LetterAvatar';
+import { Organization } from '@customTypes/organization';
+
+type Props = {
+  orgs: Array<Organization>;
+  handleSelection: (org: Organization) => void;
+  title: string;
+}; 
+
+const ListSide = ({orgs, handleSelection, title}: Props) => {
+  return (
+   <Box sx={{ width: '100%', minWidth: 500, bgcolor: 'background.paper' }}>
+
+      <Typography variant="h4" marginLeft="13px" > title  </Typography>
+      <List>
+      {orgs.map((org) => { return (
+        <ListItem disablePadding key={org.id}
+          onClick={() => { handleSelection(org) }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <LetterAvatar name={org.name}/>
+            </ListItemIcon>
+            <ListItemText primary={org.name} />
+          </ListItemButton>
+        </ListItem>
+      )})}
+      </List>
+    <Divider />
+   </Box>
+  )
+}
+
+export default ListSide;
