886ef82fd6dac56053130a5f2988b3d48ae049f6
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-27 08:57:15 -0300

    feature: Adds organizations and products lists

--- src/shared/components/Layout/SideMenu/ListSide/ListSide.tsx
+++ src/shared/components/Layout/SideMenu/ListSide/ListSide.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import {Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from '@mui/material/'
 import LetterAvatar from '@components/LetterAvatar';
 import { Organization } from '@customTypes/organization';
+import { Product } from '@customTypes/product';
 
 type Props = {
-  orgs: Array<Organization>;
-  handleSelection: (org: Organization) => void;
+  orgs: Array<any>;
+  handleSelection: (org: any) => void;
   title: string;
 }; 
 
@@ -14,7 +15,7 @@ const ListSide = ({orgs, handleSelection, title}: Props) => {
   return (
    <Box sx={{ width: '100%', minWidth: 500, bgcolor: 'background.paper' }}>
 
-      <Typography variant="h4" marginLeft="13px" > title  </Typography>
+      <Typography variant="h4" marginLeft="13px" > {title} </Typography>
       <List>
       {orgs.map((org) => { return (
         <ListItem disablePadding key={org.id}
