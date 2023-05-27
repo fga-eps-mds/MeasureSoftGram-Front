4e8cb66fedb362be40a6767bf637c06c3fd0a4dc
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-27 09:00:27 -0300

    feature: Menu grouping for all buttons

--- src/shared/components/Layout/SideMenu/SideMenuItem/SideMenuItem.tsx
+++ src/shared/components/Layout/SideMenu/SideMenuItem/SideMenuItem.tsx
@@ -1,40 +1,79 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
-import { Box, Button, Drawer, SwipeableDrawer, Tooltip } from '@mui/material';
+import { Box, Drawer, Tooltip } from '@mui/material';
 import { useSideMenuContext } from '@contexts/SidebarProvider/SideMenuProvider';
 import * as Styles from './styles';
+import { useOrganizationContext } from '@contexts/OrganizationProvider';
+import { useProductContext } from '@contexts/ProductProvider';
+import { useRepositoryContext } from '@contexts/RepositoryProvider';
+
+export type ContextControl = 'product' | 'organization' | 'repository';
 
 type Props = {
   startIcon: JSX.Element;
   endIcon?: JSX.Element;
   text: string;
   tooltip: string;
-  type : 'drawer' | 'route';
+  optype : string;
   openState: boolean;
   children?: JSX.Element;
-  handleClose: () => void;
+  handlePath?: () => void;
+  handleClose?: () => void;
+  context: ContextControl;
 };
 
-function SideMenuItem({ startIcon, text, endIcon, tooltip, type, openState, children, handleClose}: Props) {
+function SideMenuItem(
+  {
+    startIcon, text, endIcon, tooltip, optype,
+    openState, children, handleClose, context,
+    handlePath
+  }: Props) {
   const { isCollapsed } = useSideMenuContext();
+  const {currentOrganization} = useOrganizationContext();
+  const {currentProduct} = useProductContext();
+  const {currentRepository} = useRepositoryContext()
 
   const itemRef = useRef();
+  let checkContext = false;
+
+  switch (context) {
+    case 'organization':
+      checkContext = currentOrganization && true; 
+      break;
+
+    case 'product':
+      checkContext = currentOrganization && (typeof currentProduct !== undefined && typeof currentProduct !== null); 
+      break;
+
+    case 'repository':
+      checkContext = currentOrganization
+          && (typeof currentProduct !== undefined && typeof currentProduct !== null)
+          && (currentRepository !== undefined && currentRepository !== null);
+      break;
+
+    default:
+      checkContext = true;
+      break;
+  } 
   
-  return (
-    <Box onClick={() => handleClose()}
+  if ((optype === "drawer" || checkContext) ) { 
+    return <Box onClick={handleClose}
       ref={itemRef} sx={{ marginY: '2px' }}>
       <Tooltip title={tooltip} arrow placement="left">
-        <Styles.Wrapper $collapsed={isCollapsed}>
+        <Styles.Wrapper 
+          $collapsed={isCollapsed}
+          onClick={handlePath}
+        >
           <Styles.IconContainer>{startIcon}</Styles.IconContainer>
-          {!isCollapsed && (
+          {!isCollapsed &&  <>
             <>
               <Box sx={{ width: '100%', marginLeft: '10px' }}>{text}</Box>
               {endIcon && <Box sx={{ paddingRight: '10px' }}>{endIcon}</Box>}
             </>
-          )}
+          </>}
         </Styles.Wrapper>
       </Tooltip>
-      {type === 'drawer' && <Drawer
+      {optype === 'drawer' && <Drawer
         anchor='left'
         open={openState}
         onClose={handleClose}
@@ -42,7 +81,7 @@ function SideMenuItem({ startIcon, text, endIcon, tooltip, type, openState, chil
         {children}
       </Drawer>}
     </Box>
-  );
+  } else return null;
 }
 
 export default SideMenuItem;
