73d80d4273014db76be1812e0a284ab2332b1366
igorq937 <igorq937@gmail.com>
2023-05-29 10:29:42 -0300

    style: fix lint warnings and errors

--- src/shared/components/Layout/SideMenu/ListSide/ListSide.tsx
+++ src/shared/components/Layout/SideMenu/ListSide/ListSide.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
 
-import {Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from '@mui/material/'
+import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material/';
 import LetterAvatar from '@components/LetterAvatar';
-import { Organization } from '@customTypes/organization';
-import { Product } from '@customTypes/product';
 
 type Props = {
   orgs: Array<any>;
   handleSelection: (org: any) => void;
   title: string;
-}; 
+};
 
-const ListSide = ({orgs, handleSelection, title}: Props) => {
-  return (
-   <Box sx={{ width: '100%', minWidth: 500, bgcolor: 'background.paper' }}>
-
-      <Typography variant="h4" marginLeft="13px" > {title} </Typography>
-      <List>
-      {orgs.map((org) => { return (
-        <ListItem disablePadding key={org.id}
-          onClick={() => { handleSelection(org) }}>
+const ListSide = ({ orgs, handleSelection, title }: Props) => (
+  <Box sx={{ width: '100%', minWidth: 500, bgcolor: 'background.paper' }}>
+    <Typography variant="h4" marginLeft="13px">
+      {' '}
+      {title}{' '}
+    </Typography>
+    <List>
+      {orgs.map((org) => (
+        <ListItem
+          disablePadding
+          key={org.id}
+          onClick={() => {
+            handleSelection(org);
+          }}
+        >
           <ListItemButton>
             <ListItemIcon>
-              <LetterAvatar name={org.name}/>
+              <LetterAvatar name={org.name} />
             </ListItemIcon>
             <ListItemText primary={org.name} />
           </ListItemButton>
         </ListItem>
-      )})}
-      </List>
+      ))}
+    </List>
     <Divider />
-   </Box>
-  )
-}
+  </Box>
+);
 
 export default ListSide;
--- src/shared/components/Layout/SideMenu/SideMenuItem/SideMenuItem.tsx
+++ src/shared/components/Layout/SideMenu/SideMenuItem/SideMenuItem.tsx
@@ -2,11 +2,11 @@ import React, { useRef } from 'react';
 
 import { Box, Drawer, Tooltip } from '@mui/material';
 import { useSideMenuContext } from '@contexts/SidebarProvider/SideMenuProvider';
-import * as Styles from './styles';
 import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import { useProductContext } from '@contexts/ProductProvider';
 import { useRepositoryContext } from '@contexts/RepositoryProvider';
-import { useQuery } from '../../../../../pages/products/[product]/repositories/hooks/useQuery'
+import { useQuery } from '@pages/products/[product]/repositories/hooks/useQuery';
+import * as Styles from './styles';
 
 export type ContextControl = 'product' | 'organization' | 'repository';
 
@@ -15,7 +15,7 @@ type Props = {
   endIcon?: JSX.Element;
   text: string;
   tooltip: string;
-  optype : string;
+  optype: string;
   openState: boolean;
   children?: JSX.Element;
   handlePath?: () => void;
@@ -23,71 +23,71 @@ type Props = {
   context: ContextControl;
 };
 
-function SideMenuItem(
-  {
-    startIcon, text, endIcon, tooltip, optype,
-    openState, children, handleClose, context,
-    handlePath
-  }: Props) {
-
+function SideMenuItem({
+  startIcon,
+  text,
+  endIcon,
+  tooltip,
+  optype,
+  openState,
+  children,
+  handleClose,
+  context,
+  handlePath
+}: Props) {
   useQuery();
   const { isCollapsed } = useSideMenuContext();
-  const {currentOrganization} = useOrganizationContext();
-  const {currentProduct} = useProductContext();
-  const {repositoryList} = useRepositoryContext()
+  const { currentOrganization } = useOrganizationContext();
+  const { currentProduct } = useProductContext();
+  const { repositoryList } = useRepositoryContext();
 
   const itemRef = useRef();
   let checkContext = false;
-  const checkOrg  = () => (currentOrganization !== null && currentOrganization !== undefined);
-  const checkProd = () => (currentProduct !== undefined && currentProduct !== null);
-  const checkRepo = () => repositoryList !== null && repositoryList !== undefined && repositoryList?.length !==0;
+  const checkOrg = () => currentOrganization !== null && currentOrganization !== undefined;
+  const checkProd = () => currentProduct !== undefined && currentProduct !== null;
+  const checkRepo = () => repositoryList !== null && repositoryList !== undefined && repositoryList?.length !== 0;
 
   switch (context) {
     case 'organization':
-      checkContext = checkOrg(); 
+      checkContext = checkOrg();
       break;
 
     case 'product':
-      checkContext = checkOrg() && checkProd(); 
+      checkContext = checkOrg() && checkProd();
       break;
 
     case 'repository':
-      checkContext = checkOrg()
-          && checkProd()
-          && checkRepo();
+      checkContext = checkOrg() && checkProd() && checkRepo();
       break;
 
     default:
       checkContext = true;
       break;
-  } 
-  
-  if ((optype === "drawer" || checkContext) ) { 
-    return (<Box onClick={handleClose}
-        ref={itemRef} sx={{ marginY: '2px' }}>
+  }
+
+  if (optype === 'drawer' || checkContext) {
+    return (
+      <Box onClick={handleClose} ref={itemRef} sx={{ marginY: '2px' }}>
         <Tooltip title={tooltip} arrow placement="left">
-          <Styles.Wrapper 
-            $collapsed={isCollapsed}
-            onClick={handlePath}
-          >
+          <Styles.Wrapper $collapsed={isCollapsed} onClick={handlePath}>
             <Styles.IconContainer>{startIcon}</Styles.IconContainer>
-            {!isCollapsed &&  <>
+            {!isCollapsed && (
               <>
                 <Box sx={{ width: '100%', marginLeft: '10px' }}>{text}</Box>
                 {endIcon && <Box sx={{ paddingRight: '10px' }}>{endIcon}</Box>}
               </>
-            </>}
+            )}
           </Styles.Wrapper>
         </Tooltip>
-        {optype === 'drawer' && <Drawer
-          anchor='left'
-          open={openState}
-          onClose={handleClose}
-        >
-          {children}
-        </Drawer>}
-      </Box>)
-  } else return null;
+        {optype === 'drawer' && (
+          <Drawer anchor="left" open={openState} onClose={handleClose}>
+            {children}
+          </Drawer>
+        )}
+      </Box>
+    );
+  }
+  return null;
 }
 
 export default SideMenuItem;
