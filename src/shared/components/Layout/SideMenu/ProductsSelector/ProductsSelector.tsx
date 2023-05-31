bc907297c3efd76d20a4f8a73a3c275991b0715d
igorq937 <igorq937@gmail.com>
2023-05-31 13:03:15 -0300

    fix: router to overview when change the product

--- src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
+++ src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
@@ -4,12 +4,22 @@ import { FiBox, FiRepeat } from 'react-icons/fi';
 import LetterAvatar from '@components/LetterAvatar';
 import { useProductContext } from '@contexts/ProductProvider';
 import useBoolean from '@hooks/useBoolean';
+import { useRouter } from 'next/router';
+import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import SideList from '../SideList';
 import SideMenuItem from '../SideMenuItem';
 
 function ProductSelector() {
+  const { currentOrganization } = useOrganizationContext();
   const { currentProduct, setCurrentProduct, productsList } = useProductContext();
   const { value: isOpen, setTrue: onClick, setFalse: onClose } = useBoolean(false);
+  const router = useRouter();
+
+  const onClickItem = (value: any) => {
+    setCurrentProduct(value);
+    onClose();
+    router.push(`/products/${currentOrganization?.id}-${value.id}-${value.name}`);
+  };
 
   return (
     <>
@@ -20,7 +30,7 @@ function ProductSelector() {
         tooltip="Seleção de Produto"
         onClick={onClick}
       />
-      <SideList values={[...productsList]} open={isOpen} onClose={onClose} onClickItem={setCurrentProduct} />
+      <SideList values={productsList || []} open={isOpen} onClose={onClose} onClickItem={onClickItem} />
     </>
   );
 }
--- src/shared/components/Layout/SideMenu/SideMenu.tsx
+++ src/shared/components/Layout/SideMenu/SideMenu.tsx
@@ -4,12 +4,14 @@ import { useAuth } from '@contexts/Auth';
 import { useProductContext } from '@contexts/ProductProvider';
 import { useRouter } from 'next/router';
 import { useRepositoryContext } from '@contexts/RepositoryProvider';
+import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import SideMenuItem from './SideMenuItem/SideMenuItem';
 import SideMenuWrapper from './SideMenuWrapper';
 import UserMenu from './UserMenu';
 
 function SideMenu() {
   const { session } = useAuth();
+  const { currentOrganization } = useOrganizationContext();
   const { currentProduct } = useProductContext();
   const { currentRepository } = useRepositoryContext();
   const router = useRouter();
@@ -19,19 +21,21 @@ function SideMenu() {
       startIcon: <FiBarChart2 fontSize={28} />,
       text: 'Visão Geral',
       tooltip: 'Visão Geral do Produto',
-      path: `/products/${currentProduct?.id}-${currentProduct?.name}`
+      path: `/products/${currentOrganization?.id}-${currentProduct?.id}-${currentProduct?.name}`,
+      disable: !currentOrganization
     },
     {
       startIcon: <FiGitBranch fontSize={28} />,
       text: 'Repositórios',
       tooltip: 'Repositórios do Produto',
-      path: `/products/${currentProduct?.id}-${currentProduct?.name}/repositories`
+      path: `/products/${currentOrganization?.id}-${currentProduct?.id}-${currentProduct?.name}/repositories`,
+      disable: !currentProduct
     },
     {
       startIcon: <FiPaperclip fontSize={28} />,
       text: 'Releases',
       tooltip: 'Releases de cada repositório',
-      path: `/products/${currentProduct?.id}-${currentProduct?.name}/releases`,
+      path: `/products/${currentOrganization?.id}-${currentProduct?.id}-${currentProduct?.name}/releases`,
       disable: !currentRepository
     }
   ];
