9692f3d4e9fd473b3b54fe662343db2384330b3c
igorq937 <igorq937@gmail.com>
2023-05-31 11:34:55 -0300

    refactor: Organization and Product selector

--- src/shared/components/Layout/SideMenu/OrganizationSelector/OrganizationSelector.tsx
+++ src/shared/components/Layout/SideMenu/OrganizationSelector/OrganizationSelector.tsx
@@ -1,42 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { BsFillBuildingFill } from 'react-icons/bs';
 import { FiRepeat } from 'react-icons/fi';
-import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import LetterAvatar from '@components/LetterAvatar';
+import useBoolean from '@hooks/useBoolean';
+import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import SideMenuItem from '../SideMenuItem';
-import ListSide from '../ListSide/ListSide';
+import SideList from '../SideList';
 
 function OrganizationSelector() {
   const { organizationList, setCurrentOrganization, currentOrganization } = useOrganizationContext();
-  const [selectedOrganization, setSelectedOrganization] = useState<number>();
-  const [toggleOpen, setToggle] = useState(false);
-
-  /*useEffect(() => {
-    if (organizationList.length !== 0 && !currentOrganization) {
-      setSelectedOrganization(0);
-    }
-  }, [currentOrganization, organizationList]);*/
-
-  useEffect(() => {
-    if (selectedOrganization !== undefined) setCurrentOrganization(organizationList[selectedOrganization]);
-  }, [organizationList, selectedOrganization, setCurrentOrganization]);
-
-  const handleClose = () => {
-    setToggle(!toggleOpen);
-  }
+  const { value: isOpen, setTrue: onClick, setFalse: onClose } = useBoolean(false);
 
   return (
-    <SideMenuItem 
-      optype='drawer'
-      context={'organization'}
-      handleClose={handleClose} 
-      startIcon={<LetterAvatar name={currentOrganization?.name} icon={<BsFillBuildingFill />} />}
-      text={currentOrganization?.name ? currentOrganization.name : 'Selecione a organização'}
-      endIcon={<FiRepeat />}
-      tooltip="Seleção de organização"
-      openState={toggleOpen}
-      children={<ListSide title='Selecione sua Organização' orgs={organizationList} handleSelection={(item) => {setCurrentOrganization(item);}} />}
-    />
+    <>
+      <SideMenuItem
+        startIcon={<LetterAvatar name={currentOrganization?.name || '?'} icon={<BsFillBuildingFill />} />}
+        text={currentOrganization?.name || 'Selecione a Organização'}
+        endIcon={<FiRepeat />}
+        tooltip="Seleção de Organização"
+        onClick={onClick}
+      />
+      <SideList values={organizationList} open={isOpen} onClose={onClose} onClickItem={setCurrentOrganization} />
+    </>
   );
 }
 
--- src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
+++ src/shared/components/Layout/SideMenu/ProductsSelector/ProductsSelector.tsx
@@ -1,61 +1,27 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
-import { SelectChangeEvent } from '@mui/material';
 import { FiBox, FiRepeat } from 'react-icons/fi';
-import { useProductContext } from '@contexts/ProductProvider';
-import { useRouter } from 'next/router';
-import { useOrganizationContext } from '@contexts/OrganizationProvider';
 import LetterAvatar from '@components/LetterAvatar';
+import { useProductContext } from '@contexts/ProductProvider';
+import useBoolean from '@hooks/useBoolean';
+import SideList from '../SideList';
 import SideMenuItem from '../SideMenuItem';
-import ListSide from '../ListSide/ListSide';
-import { Product } from '@customTypes/product';
 
 function ProductSelector() {
-  const router = useRouter();
-  const [openClose, setToggle] = useState(false)
-  const { productsList, setCurrentProduct, currentProduct } = useProductContext();
-  const { currentOrganization } = useOrganizationContext();
-
-  const handleClose = () => {
-    setToggle(!openClose);
-  }
-  const handleChange = useCallback(
-    (event: SelectChangeEvent) => {
-      const productId = Number(event.target.value);
-      if (!productId || !productsList) return;
-      const product = productsList.find((productData) => Number(productData.id) === productId);
-      if (product) {
-        setCurrentProduct(product);
-      }
-    },
-    [currentOrganization?.id, productsList, router, setCurrentProduct]
-  );
-
-  const handleSetProduct = (item: Product) => {
-    let oldproduct = currentProduct;
-        setCurrentProduct(item);
-        if (oldproduct !== item) {
-          router.push(`/products/${currentOrganization?.id}-${item?.id}-${item?.name}`);
-        }
-  }
-
-  const getProductId = useCallback((productName: string) => Number(productName.split('-')[1]) || '', []);
+  const { currentProduct, setCurrentProduct, productsList } = useProductContext();
+  const { value: isOpen, setTrue: onClick, setFalse: onClose } = useBoolean(false);
 
   return (
-    <SideMenuItem
-      optype='drawer'
-      context={'organization'}
-      openState={openClose}
-      handleClose={handleClose}
-      startIcon={<LetterAvatar name={currentProduct? currentProduct.name : 'S'} icon={<FiBox />} />}
-      text={currentProduct? currentProduct.name : 'Selecione Um Produto'}
-      endIcon={<FiRepeat />}
-      tooltip="Seleção de produto"
-      children={<ListSide orgs={productsList || []} 
-        title='Selecione o Produto' 
-        handleSelection={handleSetProduct}
-      />}
-    />
+    <>
+      <SideMenuItem
+        startIcon={<LetterAvatar name={currentProduct?.name || '?'} icon={<FiBox />} />}
+        text={currentProduct?.name || 'Selecione o Produto'}
+        endIcon={<FiRepeat />}
+        tooltip="Seleção de Produto"
+        onClick={onClick}
+      />
+      <SideList values={[...productsList]} open={isOpen} onClose={onClose} onClickItem={setCurrentProduct} />
+    </>
   );
 }
 
