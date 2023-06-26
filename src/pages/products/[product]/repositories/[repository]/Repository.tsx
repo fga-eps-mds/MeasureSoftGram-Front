df591768268b3118f4bce630547c0a25d0acdeb2
carlosFiuza <carlosfiuza1234@gmail.com>
2023-06-26 16:51:18 -0300

    feat: add CustomTab component with characteristics charts in Repository page
    Co-authored-by: igorq937 <igorq937@gmail.com>

--- src/pages/products/[product]/repositories/[repository]/Repository.tsx
+++ src/pages/products/[product]/repositories/[repository]/Repository.tsx
@@ -5,20 +5,41 @@ import getLayout from '@components/Layout';
 import useRequireAuth from '@hooks/useRequireAuth';
 import GraphicChart from '@components/GraphicChart'
 import LatestValueTable from '@components/LatestValueTable';
+import RadarIcon from '@mui/icons-material/Radar';
+import SpeedIcon from '@mui/icons-material/Speed';
+import CustomTabs from "./components/CustomTabs";
+
 import Headers from './components/Header';
 import { useQuery } from './hooks/useQuery';
+
+
 
 const Repository: NextPageWithLayout = () => {
   useRequireAuth();
   useQuery();
 
+
+
   return (
     <Box display="flex" width="100%" flexDirection="row" marginTop="40px" marginBottom="24px">
       <Container sx={{ marginBottom: '150px' }}>
         <Headers />
-        <GraphicChart title="Histórico das Características" type='line' value="characteristics" addHistoricalSQC />
+
+        <CustomTabs
+          tabId='tab1'
+          orientation='vertical'
+          tabHeaderItems={[
+            <RadarIcon sx={{fontSize: "27px"}}/>,
+            <SpeedIcon sx={{fontSize: "27px"}}/>
+          ]}
+          tabPanelItems={[
+            <GraphicChart title="Histórico das Características" type='line' value="characteristics" addHistoricalSQC />,
+            <GraphicChart title="Histórico das Características" type='msg' value='characteristics' />
+          ]}
+        />
+
         <LatestValueTable title="Características" value="characteristics" />
-        <GraphicChart title="MSG - Características" type='msg' value='characteristics' />
+
         <GraphicChart title="Sub-Características" type='line' value="subcharacteristics" />
         <LatestValueTable title="Sub-Características" value="subcharacteristics" />
         <GraphicChart title="Medidas" type='line' value="measures" />
