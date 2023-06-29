6d62310a24a63bb652b9a2c9864fe446d5e32f05
Guilherme Braz <gvcerveirabraz@gmail.com>
2023-06-29 01:16:33 -0300

    feature: initial config page add api field
    Co-authored-by: higton <matpaulo@hotmail.com.br>

--- src/pages/config/ProfileConfig.tsx
+++ src/pages/config/ProfileConfig.tsx
@@ -2,10 +2,19 @@ import useRequireAuth from '@hooks/useRequireAuth';
 import { NextPageWithLayout } from '@pages/_app.next';
 import Head from 'next/head';
 import { getLayout } from '@components/Layout/getLayout';
-import { Container } from '@mui/material';
+import { Container, TextField, InputAdornment, IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useState } from 'react';
 
 const ProfileConfig: NextPageWithLayout = () => {
   useRequireAuth();
+
+  const [tokenVisible, setTokenVisible] = useState(false);
+  const apiToken = 'YourAPIToken'; // replace with your actual token
+
+  const handleToggleVisibility = () => {
+    setTokenVisible(!tokenVisible);
+  };
 
   return (
     <>
@@ -14,6 +23,22 @@ const ProfileConfig: NextPageWithLayout = () => {
       </Head>
       <Container>
         <h1>Configurações</h1>
+
+        <h3>Chave secreta para acesso à API</h3>
+        <TextField
+          type={tokenVisible ? 'text' : 'password'}
+          value={apiToken}
+          label="Chave secreta"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" onClick={handleToggleVisibility}>
+                  {tokenVisible ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
       </Container>
     </>
   );
