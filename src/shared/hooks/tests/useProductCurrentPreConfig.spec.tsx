2bc62be92d3b95e15decaedbc25ef624cc8d1d03
igorq937 <igorq937@gmail.com>
2023-07-09 14:39:40 -0300

    test: update useProductCurrentPreConfig

--- src/shared/hooks/tests/useProductCurrentPreConfig.spec.tsx
+++ src/shared/hooks/tests/useProductCurrentPreConfig.spec.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useProductCurrentPreConfig } from "@hooks/useProductCurrentPreConfig";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { OrganizationProvider } from "@contexts/OrganizationProvider";
 import { ProductProvider } from "@contexts/ProductProvider";
 import { RepositoryProvider } from "@contexts/RepositoryProvider";
 import ProductConfigFilterProvider from "@contexts/ProductConfigFilterProvider/ProductConfigFilterProvider";
-import useSWR from "swr";
+import api from "@services/api";
 
-jest.mock("swr")
+jest.mock("@services/api");
 
 interface Props {
   children: React.ReactNode;
@@ -25,18 +25,76 @@ const AllTheProviders = ({ children }: Props) => (
   </OrganizationProvider>
 )
 
-const data = [
+const data = {
+  "id": 57,
+  "name": "MeasureSoftGram",
+  "data": {
+    "characteristics": [
+      {
+        "key": "reliability",
+        "weight": 50,
+        "subcharacteristics": [
+          {
+            "key": "testing_status",
+            "weight": 100,
+            "measures": [
+              {
+                "key": "passed_tests",
+                "weight": 33,
+                "metrics": [
+                  {
+                    "key": "tests"
+                  },
+                  {
+                    "key": "test_failures"
+                  },
+                  {
+                    "key": "test_errors"
+                  }
+                ]
+              },
+              {
+                "key": "test_builds",
+                "weight": 20,
+                "metrics": [
+                  {
+                    "key": "test_execution_time"
+                  },
+                  {
+                    "key": "tests"
+                  }
+                ],
+                "max_threshold": 300000
+              },
+              {
+                "key": "test_coverage",
+                "weight": 47,
+                "metrics": [
+                  {
+                    "key": "coverage"
+                  }
+                ],
+                "min_threshold": 70
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "created_at": "2023-07-08T23:30:18.537901-03:00",
+  "created_config": true
+};
+
+const formattedData = [
   {
     "key": "reliability",
-    "weight": 100,
     "subcharacteristics": [
       {
         "key": "testing_status",
-        "weight": 100,
         "measures": [
           {
             "key": "passed_tests",
-            "weight": 33,
             "metrics": [
               {
                 "key": "tests"
@@ -47,11 +105,12 @@ const data = [
               {
                 "key": "test_errors"
               }
-            ]
+            ],
+            "weight": 33
           },
           {
             "key": "test_builds",
-            "weight": 20,
+            "max_threshold": 300000,
             "metrics": [
               {
                 "key": "test_execution_time"
@@ -60,36 +119,42 @@ const data = [
                 "key": "tests"
               }
             ],
-            "max_threshold": 300000
+            "weight": 20
           },
           {
             "key": "test_coverage",
-            "weight": 47,
             "metrics": [
               {
                 "key": "coverage"
               }
             ],
-            "min_threshold": 70
+            "min_threshold": 70,
+            "weight": 47
           }
-        ]
+        ],
+        "weight": 100
       }
-    ]
+    ],
+    "weight": 50
   }
-];
+]
 
 describe("useProductCurrentPreConfig", () => {
-  it("should return the current pre config", () => {
-    useSWR.mockReturnValue({
-      data,
-      error: null,
-      isValidating: false,
+  it("should return the current pre config", async () => {
+    api.get.mockResolvedValue({
+      data
     });
 
-    const { result } = renderHook(() => useProductCurrentPreConfig(), {
+    const { result, rerender } = renderHook(() => useProductCurrentPreConfig(), {
       wrapper: AllTheProviders,
     });
 
+    expect(api.get).toHaveBeenCalledTimes(1);
+
+    rerender();
+    await waitFor(() => expect(result.current.data).toEqual(formattedData));
+
+    rerender();
     expect(result).toMatchSnapshot();
   });
 })
