b7ac26a494b9511b2946dd6b7664e518ae2c957f
LeviLunique <levi.lunique@gmail.com>
2023-11-03 11:15:06 -0400

    feat(Organization): Button and modal to add members Co-authored-by: Renata Quadros <renatinha28@gmail.com>, Vitor Kühl <vitorekrlol@gmail.com>

--- src/pages/organizations/Organizations.tsx
+++ src/pages/organizations/Organizations.tsx
@@ -4,7 +4,7 @@ import { useOrganizationQuery } from './hooks/useOrganizationQuery';
 import getLayout from '@components/Layout';
 import { toast } from 'react-toastify';
 import { getAllUsers, User } from '@services/user';
-import { Container, TextField, Button, Typography, Box, Checkbox, FormControl, ListItem, List, Grid } from '@mui/material';
+import { Container, TextField, Button, Typography, Box, FormControl, ListItem, ListItemText, List, Grid, Modal, Backdrop, Fade } from '@mui/material';
 import Head from 'next/head';
 
 interface OrganizationsType extends React.FC {
@@ -17,9 +17,18 @@ const Organizations: OrganizationsType = () => {
   const [chave, setChave] = useState('');
   const [descricao, setDescricao] = useState('');
   const [membros, setMembros] = useState<string[]>([]);
+  const [openModal, setOpenModal] = useState<boolean>(false);
   const { createOrganization, getOrganizationById, updateOrganization } = useOrganizationQuery();
   const [users, setUsers] = useState<User[]>([]);
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   const carregarMembrosDaOrganizacao = async () => {
     const result = await getAllUsers();
@@ -108,8 +117,6 @@ const Organizations: OrganizationsType = () => {
     console.log('Resposta da API:', result);
   };
 
-
-
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
       <Head>
@@ -154,20 +161,9 @@ const Organizations: OrganizationsType = () => {
             <Typography variant="h6" gutterBottom sx={{ mb: 2 }}>
               Membros
             </Typography>
-            <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
-              <List sx={{ maxHeight: 200, overflowY: 'auto' }}>
-                {Array.isArray(users) && users.length > 0 ? (
-                  users.map((user) => (
-                    <ListItem key={user.id} button onClick={() => handleToggleUser(user.username)}>
-                      <Checkbox checked={membros.indexOf(user.username) > -1} />
-                      {user.first_name} {user.last_name} ({user.username})
-                    </ListItem>
-                  ))
-                ) : (
-                  <Typography>Nenhum usuário disponível</Typography>
-                )}
-              </List>
-            </FormControl>
+            <Button variant="contained" color="primary" onClick={handleOpenModal} sx={{ mb: 2 }}>
+              Adicionar Membros
+            </Button>
           </Grid>
           <Grid item xs={12}>
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -182,6 +178,55 @@ const Organizations: OrganizationsType = () => {
           </Grid>
         </Grid>
       </form>
+
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openModal}>
+          <Box sx={{ width: 400, p: 3, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'background.paper', boxShadow: 24, borderRadius: 1 }}>
+            <Typography variant="h6" gutterBottom>
+              Adicionar Membros
+            </Typography>
+            <FormControl fullWidth variant="outlined">
+              <List sx={{ maxHeight: '300px', overflowY: 'auto' }}>
+                {Array.isArray(users) && users.length > 0 ? (
+                  users.map((user) => {
+                    const isMember = membros.indexOf(user.username) > -1;
+                    return (
+                      <ListItem key={user.id}>
+                        <ListItemText>
+                          {user.first_name} {user.last_name} ({user.username})
+                        </ListItemText>
+                        <Button
+                          variant={isMember ? "outlined" : "contained"}
+                          color="primary"
+                          onClick={() => handleToggleUser(user.username)}
+                          sx={{ opacity: isMember ? 0.5 : 1 }}
+                        >
+                          {isMember ? 'Adicionado' : 'Adicionar'}
+                        </Button>
+                      </ListItem>
+                    );
+                  })
+                ) : (
+                  <Typography>Nenhum usuário disponível</Typography>
+                )}
+              </List>
+            </FormControl>
+            <Box sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
+              <Button color="primary" onClick={handleCloseModal}>
+                Fechar
+              </Button>
+            </Box>
+          </Box>
+        </Fade>
+      </Modal>
     </Container>
   );
 };
