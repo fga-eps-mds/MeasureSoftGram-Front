2c0cf335f85be10e8aaaba04de8d503946f6f307
LucasGlopes <lucasglpes@gmail.com>
2023-11-14 12:07:42 -0300

    feat: add gauge parameters modal
    Co-authored-by: IanPSRocha <ianrocha.df@gmail.com>

---
+++ src/pages/products/[product]/repositories/[repository]/components/GaugeSlider/GaugeSlider.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Box, Slider } from "@mui/material";
+
+interface GaugeSliderProps {
+  initialValues: number[];
+  min: number;
+  max: number;
+  values: number[];
+  setValues: (newValues: number[]) => void;
+  step: number;
+}
+
+function GaugeSlider(props: GaugeSliderProps) {
+  const {
+    initialValues,
+    min,
+    max,
+    values,
+    setValues,
+    step
+  } = props;
+
+  const [perc, setPerc] = useState(
+    initialValues.map((val: number) => (val / max) * 100)
+  );
+
+  const onChange = (e: Event, tValues: number | number[]) => {
+    if (Array.isArray(tValues)) {
+      const [minVal, maxVal] = tValues;
+      if (maxVal > minVal && maxVal !== minVal && minVal > 0 && maxVal < 1) {
+        setValues(tValues);
+        setPerc(tValues.map((val: number) => (val / max) * 100));
+      }
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Slider
+        disableSwap
+        sx={{
+          width: '90%',
+          "& .MuiSlider-track": {
+            background: "#f1c40f",
+            border: 0
+          },
+          "& .MuiSlider-thumb": {
+            [`&:nth-of-type(${1}n)`]: {
+              background: "#313131",
+              "& span": {
+                background: "#313131",
+                borderRadius: '4px',
+              }
+            },
+            [`&:nth-of-type(${2}n)`]: {
+              background: "#313131",
+              "& span": {
+                background: "#313131",
+                borderRadius: '4px',
+              }
+            }
+          },
+          "& .MuiSlider-mark": {
+            background: "none"
+          },
+          "& .MuiSlider-rail": {
+            opacity: 100,
+            border: 0,
+            background: `linear-gradient(to right, #e74c3c 0% ${perc[0]}%, #f1c40f ${perc[0]}% ${perc[1]}%, #07bc0c ${perc[1]}% 100%)`
+          }
+        }}
+        valueLabelDisplay="auto"
+        value={values}
+        min={min}
+        max={max}
+        scale={(x) => (x)}
+        marks={[
+          { value: min, label: min },
+          ...perc.map((val: any) => ({
+            value: val,
+            label: val === 0 ? min : val === 1 ? max : undefined
+          })),
+          { value: max, label: max }
+        ]}
+        onChange={onChange}
+        step={step}
+      />
+    </Box>
+  );
+}
+
+export default GaugeSlider;
--- src/pages/products/[product]/repositories/[repository]/components/Header/Header.tsx
+++ src/pages/products/[product]/repositories/[repository]/components/Header/Header.tsx
@@ -1,24 +1,158 @@
 import { useRepositoryContext } from '@contexts/RepositoryProvider';
-import { Box, Typography } from '@mui/material';
+import { Alert, Box, Button, IconButton, Modal, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 
-import React from 'react';
+import React, { useState } from 'react';
+import GaugeSlider from '../GaugeSlider';
+import { useProductContext } from '@contexts/ProductProvider';
+import { useOrganizationContext } from '@contexts/OrganizationProvider';
+import ReactEcharts from 'echarts-for-react';
 
 function Header() {
   const { currentRepository } = useRepositoryContext();
+  const { currentProduct } = useProductContext();
+  const { currentOrganization } = useOrganizationContext();
+
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const initialValues = [0.33, 0.66];
+  const [values, setValues] = useState(
+    initialValues
+  );
+
+  const gaugeOptionWithoutPointer = {
+    type: 'gauge',
+    startAngle: 180,
+    endAngle: 360,
+    min: 0,
+    max: 1,
+    axisLine: {
+      lineStyle: {
+        width: 50,
+        color: [
+          [values[0], '#e74c3c'],
+          [values[1], '#f1c40f'],
+          [1, '#07bc0c']
+        ]
+      }
+    },
+    axisTick: {
+      length: 0
+    },
+    splitLine: {
+      length: 0
+    },
+    axisLabel: {
+      distance: 0,
+      rotate: 'tangential',
+      formatter: function () {
+        return '';
+      }
+    },
+  };
+
+  const option = {
+    series: [gaugeOptionWithoutPointer]
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const onSubmit = () => {
+    const data = {
+      name: currentProduct?.name,
+      redGaugeLimit: values[0],
+      yellowGaugeLimit: values[1]
+    }
+    console.log(data)
+    handleCloseModal();
+  };
 
   return (
-    <Box display="flex" flexDirection="column">
-      <Box display="flex">
-        <Typography variant="h4" marginRight="10px">
-          Repositório
-        </Typography>
-        <Typography variant="h4" fontWeight="500" color="#33568E">
-          {currentRepository?.name}
+    <Box display="flex" flexDirection="row" justifyContent="space-between">
+      <Box display="flex" flexDirection="column">
+        <Box display="flex">
+          <Typography variant="h4" marginRight="10px">
+            Repositório
+          </Typography>
+          <Typography variant="h4" fontWeight="500" color="#33568E">
+            {currentRepository?.name}
+          </Typography>
+        </Box>
+        <Typography variant="caption" color="gray">
+          {currentRepository?.description}
         </Typography>
       </Box>
-      <Typography variant="caption" color="gray">
-        {currentRepository?.description}
-      </Typography>
+      <Box>
+        <IconButton onClick={handleOpenModal}>
+          <SettingsIcon />
+        </IconButton>
+      </Box>
+
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+      >
+        <Box
+          sx={{
+            width: 550,
+            p: 2,
+            position: 'absolute',
+            top: '50%', left: '50%',
+            transform: 'translate(-50%, -50%)',
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            borderRadius: 1,
+          }}
+        >
+          <Typography variant="h6" gutterBottom>
+            Editar Intervalos
+          </Typography>
+          <Box
+            style={{
+              height: '180px',
+            }}
+          >
+            <ReactEcharts option={option} />
+          </Box>
+          <Box
+          >
+            <GaugeSlider
+              step={0.01}
+              initialValues={values}
+              min={0}
+              max={1}
+              values={values}
+              setValues={setValues}
+            />
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            gap="20px"
+            marginBottom='10px'
+          >
+            <Button
+              onClick={handleCloseModal}
+              variant='outlined'
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant='contained'
+              onClick={onSubmit}
+            >
+              Salvar
+            </Button>
+          </Box>
+          <Alert sx={{ display: "flex", justifyContent: "center", textAlign: 'center' }} severity="warning">Atenção: Essa configuração será aplicada a todos os repositórios do produto.</Alert>
+        </Box>
+      </Modal>
     </Box>
   );
 }
