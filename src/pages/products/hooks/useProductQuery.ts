035e7712be900feeb5385ed4521f000a2f56bb0f
LuaMedeiros <luanalm02@gmail.com>
2023-11-16 18:57:18 -0300

    feat: Create delete method
    Co-authored-by: Caio <caiocesar31951910@hotmail.com>
    Co-authored-by: Lucas <lucasfreirelopes9@gmail.com>

--- src/pages/products/hooks/useProductQuery.ts
+++ src/pages/products/hooks/useProductQuery.ts
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useProductContext } from '@contexts/ProductProvider';
+import { toast } from 'react-toastify';
 import { ProductFormData, productQuery } from '@services/product';
-import { useState } from 'react';
+import { Product } from '@customTypes/product';
+import api from '@services/api';
+
+// interface CurrentProductType extends Product {
+//   id: string;
+// }
+
+// interface ProductWithId extends ProductFormData {
+//   id?: string;
+// }
 
 export const useProductQuery = () => {
+  // const { currentProducts, setCurrentProducts } = useProductContext();
+
+  // const loadCurrentProducts = async () => {
+  //   try {
+  //     const responseConfig = await productQuery.getAllProduct();
+
+  //     const response = await api.request(responseConfig);
+
+  //     const products = response.data.results.map((item: ProductWithId) => ({
+  //       ...item,
+  //       id: item.id || 'fake-id'
+  //     })) as CurrentProductType[];
+
+  //     setCurrentProducts(products);
+  //   } catch (error: any) {
+  //     console.error("Erro detalhado:", error);
+  //     toast.error(`Erro ao carregar produtos: ${error.message || 'Erro desconhecido'}`);
+  //   }
+  // };
+
   const [update, setUpdate] = useState<number>(0);
 
   const createProduct = async (data: ProductFormData): Promise<Result<ProductFormData>> => {
@@ -12,7 +44,32 @@ export const useProductQuery = () => {
     return result;
   };
 
+  // const getProductById = async (id: string): Promise<Result<ProductFormData>> =>
+  // productQuery.getProductById(id);
+
+  // const updateProduct = async (id: string, data: ProductFormData): Promise<Result<void>> =>
+  // productQuery.updateProduct(id, data);
+
+  const deleteProduct = async (productId: string, organizationId: string | undefined): Promise<Result<void>> => {
+    const result = await productQuery.deleteProduct(productId, organizationId);
+    if (result.type === 'success') {
+        setUpdate((prev: number) => prev + 1);
+    }
+    return result;
+};
+
+  // useEffect(() => {
+  //   if (!currentProducts || currentProducts.length === 0 || update > 0) {
+  //     loadCurrentProducts();
+  //     if (update > 0) setUpdate(0);
+  //   }
+  // }, [currentProducts, update]);
+
+
   return {
-    createProduct
+    createProduct,
+    // getProductById,
+    // updateProduct,
+    deleteProduct
   };
 };
