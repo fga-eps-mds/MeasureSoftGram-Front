1f7b325b3ba42ce8d955a281e210ccee68cf2dc1
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-11-20 22:34:04 -0300

    feat: Fix errors to deploy

--- src/pages/products/hooks/useProductQuery.ts
+++ src/pages/products/hooks/useProductQuery.ts
@@ -1,13 +1,8 @@
-import { useState } from 'react';
 import { ProductFormData, productQuery } from '@services/product';
-export const useProductQuery = () => {
-  const [update, setUpdate] = useState<number>(0);
 
+export const useProductQuery = () => {
   const createProduct = async (data: ProductFormData): Promise<Result<ProductFormData>> => {
     const result = await productQuery.createProduct(data);
-    if (result.type === 'success') {
-      setUpdate((prev: number) => prev + 1);
-    }
     return result;
   };
 
@@ -19,9 +14,6 @@ export const useProductQuery = () => {
 
   const deleteProduct = async (productId: string, organizationId: string | undefined): Promise<Result<void>> => {
     const result = await productQuery.deleteProduct(productId, organizationId);
-    if (result.type === 'success') {
-      setUpdate((prev: number) => prev + 1);
-    }
     return result;
   };
   return {
