b5f47930550c32b26c4c95751990fe2b40d0d6e0
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-11-20 22:42:02 -0300

    feat: Fix errors to deploy

--- src/pages/products/hooks/useProductQuery.ts
+++ src/pages/products/hooks/useProductQuery.ts
@@ -1,9 +1,8 @@
 import { ProductFormData, productQuery } from '@services/product';
 
 export const useProductQuery = () => {
-  const createProduct = async (data: ProductFormData): Promise<Result<ProductFormData>> => {
-    return await productQuery.createProduct(data);
-  };
+  const createProduct = async (data: ProductFormData): Promise<Result<ProductFormData>> =>
+    productQuery.createProduct(data);
 
   const getProductById = async (organizationId: string, productId: string): Promise<Result<ProductFormData>> =>
     productQuery.getProductById(organizationId, productId);
@@ -11,9 +10,9 @@ export const useProductQuery = () => {
   const updateProduct = async (productId: string, data: ProductFormData): Promise<Result<ProductFormData>> =>
     productQuery.updateProduct(productId, data);
 
-  const deleteProduct = async (productId: string, organizationId: string | undefined): Promise<Result<void>> => {
-    return await productQuery.deleteProduct(productId, organizationId);
-  };
+  const deleteProduct = async (productId: string, organizationId: string | undefined): Promise<Result<void>> =>
+    productQuery.deleteProduct(productId, organizationId);
+
   return {
     createProduct,
     getProductById,
