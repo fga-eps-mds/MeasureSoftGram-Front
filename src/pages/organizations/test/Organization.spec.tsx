2c205ccd89b7055a140bc691ce7a24fd0b5d2460
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-11-28 20:14:24 -0300

    feat: add tests to organization crud Co-authored-by: Vitor <vitorekrlol@gmail.com>

---
+++ src/pages/organizations/test/Organization.spec.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Organizations from '../Organizations';
+import { OrganizationProvider } from '@contexts/OrganizationProvider';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+jest.mock('@services/user', () => ({
+  getAllUsers: jest.fn(() => Promise.resolve({
+    type: 'success',
+    value: {
+      results: [
+        { id: '1', first_name: 'Test', last_name: 'User', username: 'testuser' },
+      ],
+    },
+  })),
+}));
+
+jest.mock('react-toastify', () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+  },
+}));
+
+jest.mock('../hooks/useOrganizationQuery', () => ({
+  useOrganizationQuery: () => ({
+    createOrganization: jest.fn(() => Promise.resolve({ type: 'success' })),
+    getOrganizationById: jest.fn(() => Promise.resolve({ type: 'success', value: {/* organization data */ } })),
+    updateOrganization: jest.fn(() => Promise.resolve({ type: 'success' })),
+  }),
+}));
+
+describe('Organizations Component', () => {
+  const useRouter = jest.fn();
+  useRouter.mockReturnValue({ query: { edit: '1' } });
+
+  it('renders the component with initial state', () => {
+    render(
+      <OrganizationProvider>
+        <RouterContext.Provider value={{ useRouter }}>
+          <Organizations />
+        </RouterContext.Provider>
+      </OrganizationProvider>
+    );
+  });
+
+  it('handles user interactions and form submission', async () => {
+    const mockGetAllUsers = jest.fn(() => Promise.resolve({
+      type: 'success',
+      value: {
+        results: [
+          { id: '1', first_name: 'Test', last_name: 'User', username: 'testuser' },
+        ],
+      },
+    }));
+    jest.mock('@services/user', () => ({
+      getAllUsers: mockGetAllUsers,
+    }));
+
+    render(
+      <OrganizationProvider>
+        <RouterContext.Provider value={{ useRouter }}>
+          <Organizations />
+        </RouterContext.Provider>
+      </OrganizationProvider>
+    );
+  });
+});
