058b16a7949dd3763daebfc4d9408a12e369e648
LamequeFernandes <lamequesao@gmail.com>
2023-11-27 21:19:36 -0300

    fix: endpoint return typing correction and function see more of the release history

--- src/pages/products/[product]/releases/util/filterReleaseList.ts
+++ src/pages/products/[product]/releases/util/filterReleaseList.ts
@@ -1,19 +1,16 @@
-import { CompareGoalAccomplished } from '@customTypes/product';
+import { IReleases, ReleasesPaginated } from '@customTypes/product';
 
-function filterReleaseList(
-  releases: CompareGoalAccomplished[],
-  name: string,
-  startDate: string,
-  endDate: string
-): CompareGoalAccomplished[] {
-  return releases.filter((release) => {
-    const endDateObject = new Date(endDate);
-    endDateObject.setUTCHours(23, 59, 59);
-    const hasName = !name || release.release_name.toLowerCase().includes(name.toLowerCase());
-    const isAfterStartDate = !startDate || new Date(release.start_at) >= new Date(startDate);
-    const isBeforeEndDate = !endDate || new Date(release.end_at) <= endDateObject;
-    return isAfterStartDate && hasName && isBeforeEndDate;
-  });
+function filterReleaseList(releases: ReleasesPaginated, name: string, startDate: string, endDate: string): IReleases[] {
+  return (
+    releases.results?.filter((release) => {
+      const endDateObject = new Date(endDate);
+      endDateObject.setUTCHours(23, 59, 59);
+      const hasName = !name || release.release_name.toLowerCase().includes(name.toLowerCase());
+      const isAfterStartDate = !startDate || new Date(release.start_at) >= new Date(startDate);
+      const isBeforeEndDate = !endDate || new Date(release.end_at) <= endDateObject;
+      return isAfterStartDate && hasName && isBeforeEndDate;
+    }) || []
+  );
 }
 
 export default filterReleaseList;
