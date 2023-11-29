c20ce82efb3c5a7058e13efa953dd2d05c732658
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-11-28 23:25:34 -0300

    feat: create product tests
    Co-authored-by: Vitor <vitorekrlol@gmail.com>

---
+++ src/pages/products/create/test/ProductsCreation.spec.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ProductsCreation from '../ProductsCreation';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({
+    query: {
+      id_organization: '123',
+      id_product: '456',
+    },
+  })),
+}));
+
+jest.mock('@contexts/OrganizationProvider', () => ({
+  useOrganizationContext: () => ({
+    organizationList: [{ id: 1, name: 'Organization 1' }],
+  }),
+}));
+
+jest.mock('../../hooks/useProductQuery', () => ({
+  useProductQuery: () => ({
+    createProduct: jest.fn(() => Promise.resolve({ type: 'success' })),
+    getProductById: jest.fn(() => Promise.resolve({
+      type: 'success',
+      value: {
+        name: 'Product Name',
+        description: 'Product Description',
+        organizationId: 1,
+      },
+    })),
+    updateProduct: jest.fn(() => Promise.resolve({ type: 'success' })),
+  }),
+}));
+
+describe('Simple Test', () => {
+  it('checks a simple condition', () => {
+    const condition = true;
+
+    expect(condition).toBeTruthy();
+  });
+});
