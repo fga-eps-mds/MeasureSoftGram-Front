e7c4a715713711d48d2c0213682351905c17704e
vitorekr <vitorekrlol@gmail.com>
2023-12-06 16:53:20 -0300

    Revert "back tests"
    This reverts commit 578f1c2413597cff1dc68caa7c65c727ed511cec.

--- src/pages/products/test/Products.spec.tsx
+++ src/pages/products/test/Products.spec.tsx
@@ -10,6 +10,21 @@ jest.mock('next/router', () => ({
   })
 }));
 
+const mockProductsList = [
+  { id: 'prodtest', name: 'prodname', description: 'proddesc', github_url: 'https://test.github.com', created_at: '2022-01-01', updated_at: '2022-01-02' },
+  { id: 'prodtest2', name: 'prodname2', description: 'proddesc2', github_url: 'https://test.github.com/proj2', created_at: '2022-02-01', updated_at: '2022-02-02' }
+];
+
+const mockOrganizationList = [
+  { id: 1, name: 'Organization 1' },
+  { id: 2, name: 'Organization 2' }
+];
+
+const mockCurrentOrganization = {
+  id: '1',
+  name: 'Organization 1'
+}
+
 describe('Products', () => {
   describe('Snapshot', () => {
     it('Deve corresponder ao Snapshot', () => {
@@ -34,5 +49,43 @@ describe('Products', () => {
 
       expect(getByTestId('organization-title')).toBeDefined();
     });
+
+    it('renders without crashing', () => {
+      const { getByText } = render(
+        <OrganizationProvider>
+          <ProductProvider>
+            <Products />
+          </ProductProvider>
+        </OrganizationProvider>
+      );
+
+      expect(getByText('Organizações')).toBeInTheDocument();
+    });
+
+    it('resets the current organization when the same organization is clicked', async () => {
+      const mockSetCurrentOrganizations = jest.fn();
+
+      jest.mock('@contexts/OrganizationProvider', () => ({
+        useOrganizationContext: () => ({
+          organizationList: mockOrganizationList,
+          currentOrganization: mockOrganizationList[0], // The first org is selected
+          setCurrentOrganizations: mockSetCurrentOrganizations,
+        }),
+      }));
+
+      const { getByText } = render(
+        <OrganizationProvider>
+          <ProductProvider>
+            <Products />
+          </ProductProvider>
+        </OrganizationProvider>
+      );
+
+      const organizationButton = getByText(mockOrganizationList[0].name);
+      fireEvent.click(organizationButton);
+      expect(mockSetCurrentOrganizations).toHaveBeenCalledWith([]); // Expect to reset the selection
+    });
+
+
   });
 });
