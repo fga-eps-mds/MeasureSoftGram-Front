ee6cd47d3f54e011cfd334e0fdecf0677688223e
vitorekr <vitorekrlol@gmail.com>
2023-12-06 16:02:21 -0300

    test:improve product skeleton tests
    Co-authored-by: Caio Oliveira <caiocesar31951910@hotmail.com>

--- src/pages/products/components/Skeleton/test/Skeleton.spec.tsx
+++ src/pages/products/components/Skeleton/test/Skeleton.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Skeleton from '../Skeleton';
 
 describe('Skeleton', () => {
@@ -9,6 +10,13 @@ describe('Skeleton', () => {
         <Skeleton />
       );
       expect(tree).toMatchSnapshot();
+    });
+
+    it('verifica as propriedades do primeiro elemento Shimmer', () => {
+      const { getByTestId } = render(<Skeleton />);
+      const primeiroShimmer = getByTestId('primeiro-shimmer');
+      expect(primeiroShimmer).toHaveStyle('height: 65px');
+      expect(primeiroShimmer).toHaveStyle('width: 300px');
     });
   });
 });
