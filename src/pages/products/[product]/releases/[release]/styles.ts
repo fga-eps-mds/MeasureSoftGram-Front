01d199aace918b9014429701761dc5bd299ed643
LamequeFernandes <lamequesao@gmail.com>
2023-12-09 03:08:51 -0300

    refact: css adjustment for planned x accomplished chart container

--- src/pages/products/[product]/releases/[release]/styles.ts
+++ src/pages/products/[product]/releases/[release]/styles.ts
@@ -5,4 +5,8 @@ export const ContainerGraph = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 20px;
+  border-radius: 20px;
+  background-color: #fff;
+  padding: 25px;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
 `;
