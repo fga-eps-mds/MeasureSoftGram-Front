306e0869a524a37362538fbafa7f7bd6d70210d8
LamequeFernandes <lamequesao@gmail.com>
2023-12-09 02:56:26 -0300

    feat: initial version of the graphic component for planned and realized release result

---
+++ src/pages/products/[product]/releases/[release]/components/CurveGraph/CurveGraph.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+export interface CurveGraphProps {
+  planejado: number[];
+  realizado?: number[];
+  labels: string[];
+}
+
+export default function SimpleLineChart({ planejado, realizado, labels }: CurveGraphProps) {
+  const series: any[] = [
+    { data: planejado, label: 'Planejado', color: '#33568E', }
+  ]
+
+  if (realizado) {
+    series.push({ data: realizado, label: 'Realizado', color: 'red', },)
+
+    if (planejado.length !== realizado.length) {
+      return (
+        <div>
+          <p>O número do resultados das caracteristicas planejadas deve ser igual ao número de realizadas realizadas</p>
+        </div>
+      );
+    }
+  }
+
+  if (labels.length !== planejado.length) {
+    return (
+      <div>
+        <p>Quantidade de caracteristicas incompativel com o número de resultados</p>
+      </div>
+    );
+  }
+
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      series={series}
+      xAxis={[{
+        scaleType: 'point',
+        data: labels,
+        min: 0.0,
+        max: 1,
+        tickMinStep: 0,
+      }]}
+      yAxis={[
+        {
+          scaleType: 'linear',
+          min: 0.0,
+          max: 1,
+        },
+      ]}
+    />
+  );
+}
