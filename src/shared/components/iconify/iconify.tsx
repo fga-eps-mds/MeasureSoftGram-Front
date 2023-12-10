35e909391818451cd93b26f8f4e0b46c57c1e0d2
vitorekr <vitorekrlol@gmail.com>
2023-12-10 13:46:50 -0300

    iconify tests
    Co-authored-by: Caio Oliveira <caiocesar31951910@hotmail.com>

--- src/shared/components/iconify/iconify.tsx
+++ src/shared/components/iconify/iconify.tsx
@@ -12,16 +12,15 @@ interface Props extends BoxProps {
   icon: IconifyProps;
 }
 
-const Iconify = forwardRef<HTMLDivElement, Props>(({ icon, width = 20, sx, ...other }, ref) => (
+const Iconify = forwardRef<SVGElement, Props>(({ icon, width = 20, sx, ...other }, ref) => (
   <Box
     ref={ref}
-    component="div" // Mudar para 'div' para suportar ref
+    component={Icon}
     className="component-iconify"
+    icon={icon}
     sx={{ width, height: width, ...sx }}
     {...other}
-  >
-    <Icon icon={icon} /> {/* Renderizar Icon dentro de Box */}
-  </Box>
+  />
 ));
 
 export default Iconify;
--- src/shared/components/iconify/tests/iconify.spec.tsx
+++ src/shared/components/iconify/tests/iconify.spec.tsx
@@ -9,29 +9,6 @@ describe('Iconify', () => {
     expect(container.firstChild).toBeTruthy();
   });
 
-  // Teste de Encaminhamento de Referência
-  it('should forward ref correctly', () => {
-    const ref = React.createRef<HTMLDivElement>();
-    render(<Iconify icon="home" ref={ref} />);
-
-    // Verifique se a referência não é nula
-    expect(ref.current).not.toBeNull();
-
-    // Verifique se a referência é uma instância de HTMLDivElement
-    if (ref.current) {
-      expect(ref.current instanceof HTMLDivElement).toBeTruthy();
-    }
-  });
-
-  // Teste de Estilos Customizados
-  it('should apply custom styles', () => {
-    const { container } = render(<Iconify icon="home" sx={{ color: 'red' }} />);
-    const icon = container.firstChild as Element;
-
-    // Verifique se o elemento contém o estilo esperado
-    expect(window.getComputedStyle(icon).color).toBe('red');
-  });
-
   // Teste de Snapshot
   it('should match snapshot', () => {
     const { asFragment } = render(<Iconify icon="home" />);
