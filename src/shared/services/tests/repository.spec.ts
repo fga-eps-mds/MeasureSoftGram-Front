1b6941c5b6431b02c9acf1a302c5e21a3b25e4f7
vitorekr <vitorekrlol@gmail.com>
2023-12-09 22:45:43 -0300

    repository.ts test
    Co-authored-by: Caio Oliveira <caiocesar31951910@hotmail.com>

--- src/shared/services/tests/repository.spec.ts
+++ src/shared/services/tests/repository.spec.ts
@@ -33,5 +33,43 @@ describe('Repository', () => {
     });
   });
 
-  // Adicione mais testes para os outros métodos da classe Repository
+  describe('updateRepository', () => {
+    it('should handle an error when updating a repository', async () => {
+      // Simule uma falha na API
+      mockedAxios.put.mockRejectedValue(new Error('Failed to update repository'));
+
+      // Execute o método
+      const result = await repository.updateRepository('org1', 'prod1', 'repo1', { name: 'Updated Repo', platform: 'GitLab' });
+
+      // Verifique o resultado
+      expect(result.type).toEqual('error');
+    });
+  });
+
+  describe('deleteRepository', () => {
+    it('should handle an error when deleting a repository', async () => {
+      // Simule uma falha na API
+      mockedAxios.delete.mockRejectedValue(new Error('Failed to delete repository'));
+
+      // Execute o método
+      const result = await repository.deleteRepository('org1', 'prod1', 'repo1');
+
+      // Verifique o resultado
+      expect(result.type).toEqual('error');
+    });
+  });
+
+  describe('getHistoricalData', () => {
+      it('should handle an error when fetching historical data', async () => {
+      // Simule uma falha na API
+      mockedAxios.get.mockRejectedValue(new Error('Failed to fetch historical data'));
+
+      // Execute o método
+      const result = await repository.getHistoricalData({ organizationId: 'org1', productId: 'prod1', repositoryId: 'repo1', entity: 'commits' });
+
+      // Verifique o resultado
+      expect(result.type).toEqual('error');
+    });
+  });
+
 });
